64fa2aa91a4e8a5a3d37081b9e22cd99fb9959fb
Daniele Lenzi <dany@QuBit.fritz.box>
2020-01-15 01:41:41 +0100

    3 commit

---
+++ charts/.dart_tool/flutter_build/c60501128bb0a1f1eb3f9a42f488b299/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ui' as ui;
+
+import "file:///Users/dany/Documents/projects/Sandbox/flutter/SuperQubit.github.io/charts/lib/main.dart" as entrypoint;
+
+Future<void> main() async {
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
